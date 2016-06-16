57bb1a65a90374f6f00137ca15e3b618c323f6e8
PaulMartin2501 <paul.martin@thinkingbig.net>
2016-06-16 11:30:45 -0300

    Added new contact page
    Added new contact page and did some minor fixes/still issues with the
    contact page but skeleton is there

---
+++ InfiniList/InfiniList/Models/ContactForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace InfiniList.Models
+{
+    public class ContactForm
+    {
+        [Required, Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required, Display(Name = "Email"), EmailAddress]
+        public string Email { get; set; }
+
+        [Required, Display(Name = "Subject")]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
